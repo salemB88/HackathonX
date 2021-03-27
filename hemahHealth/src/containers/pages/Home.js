901e8db35e29bb09a48ac5d90f185027beea5bc0
omar-bakhash <omar.k.b222@gmail.com>
2021-03-27 11:38:01 +0300

    home screen start

---
+++ hemahHealth/src/containers/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Form } from 'react-bootstrap';
+import { Button } from '@material-ui/core';
+
+import axios from 'axios';
+
+
+import { BrowserRouter as Router ,Route,Switch,Link} from 'react-router-dom';
+
+
+export default function Home() {
+
+
+    return(
+<div>Home page</div>
+
+    );};
--- hemahHealth/src/sandBox.js
+++ hemahHealth/src/sandBox.js
@@ -3,5 +3,6 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(json => console.log(json))
 
 
-  // <Router>{/* </Router> */}
+  // <Router></Router>
+  
 // import { BrowserRouter as Router ,Route,Switch,Link} from 'react-router-dom'
